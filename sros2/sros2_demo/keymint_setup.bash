8aba52a71c19661850acf03bade5a0c9970a1d6e
ruffsl <roxfoxpox@gmail.com>
2018-08-23 20:06:32 -0700

    Simplifying keymint install

--- sros2/sros2_demo/keymint_setup.bash
+++ sros2/sros2_demo/keymint_setup.bash
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 rm -rfv demo_keys
 keymint keystore init --bootstrap keymint_ros
+
 keymint keystore create_pkg talker
 keymint keystore create_pkg listener
+keymint keystore create_pkg launch_ros
+keymint keystore create_pkg add_two_ints_client
+keymint keystore create_pkg add_two_ints_server
+
 keymint keystore build_pkg src/talker
 keymint keystore build_pkg src/listener
+keymint keystore build_pkg src/launch_ros
+keymint keystore build_pkg src/add_two_ints_client
+keymint keystore build_pkg src/add_two_ints_server
 
-find install/ -type d -exec cp -v public/* {} \;
-mv -Tv install demo_keys
+# cp -LRTv install demo_keys
+# mv -Tv install demo_keys
+ln -sv install demo_keys
