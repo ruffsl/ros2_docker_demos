382c806cc3dee3db23b41a4a82d8e08425ae67d1
ruffsl <roxfoxpox@gmail.com>
2018-11-14 14:59:48 -0800

    Changing example to build from source PRs

--- sros2/ros2_entrypoint.sh
+++ sros2/ros2_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # setup ros2 environment
-source "/opt/ros/$ROS2_DISTRO/setup.bash"
 source "/opt/rti/rti_connext_dds-$RTI_CONNEXT_VERSION/resource/scripts/rtisetenv_x64Linux3gcc$RTI_GCC_VERSION.bash"
+source "$ROS2_WS/install/local_setup.bash"
 exec "$@"
