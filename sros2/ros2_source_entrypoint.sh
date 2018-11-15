d802ee3332532af4b4b4566ec30ad9a076752a92
ruffsl <roxfoxpox@gmail.com>
2018-11-14 21:18:44 -0800

    tweeking build adding sros2 to build set connext for talker correct entrypoint file name

--- sros2/ros2_source_entrypoint.sh
+++ sros2/ros2_source_entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 # setup ros2 environment
 source "/opt/rti/rti_connext_dds-$RTI_CONNEXT_VERSION/resource/scripts/rtisetenv_x64Linux3gcc$RTI_GCC_VERSION.bash"
-source "$ROS2_WS/install/local_setup.bash"
+source "$ROS2_WS/install/setup.bash"
 exec "$@"
